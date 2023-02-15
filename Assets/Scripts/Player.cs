b18ae33349d9ab3e1cb1269e4d9f08e6b1c9f2a2
Sobhan Bahrami <ednqor@inf.elte.hu>
2023-02-15 02:50:39 +0100

    Add Collision Detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,7 +14,53 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * (moveSpeed * Time.deltaTime);
+
+        // Collision Detection
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,playerRadius,moveDir,moveDistance);
+        
+        // hug the collision diagonally and move
+
+        if (!canMove)
+        {
+            // Attempt only x movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+                playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                // can move only on the x
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // cannot move only on the x
+                
+                // attempt only z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+                    playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    // can move only on the Z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // cannot move in any direction
+                }
+            }
+        }
+
+        
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
         
         isWalking = moveDir != Vector3.zero;
         
